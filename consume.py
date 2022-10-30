e2941e0707d1feac2b00fb80fb588bc12d909849
kglavin <kglavin@gmail.com>
2022-10-30 11:24:26 -0700

    added verify

--- consume.py
+++ consume.py
@@ -2,30 +2,44 @@ import datetime
 import json
 import time
 
-
+min_conversions = {
+59: 0,
+0: 0, 
+1: 0,
+14: 15,
+15: 15, 
+16: 15,
+29: 30,
+30: 30,
+31: 30,
+44: 45, 
+45: 45,
+46: 45
+} 
 
 sch = {}
-with open('gti-1/gti-1.json', 'r') as f:
+with open('gti-1/control.json', 'r') as f:
   sch = json.loads(f.read())
+
 
 print(sch)
 
 old_key = ""
 count = 0
 while True:
-    time.sleep(1)
+    time.sleep(31)
     count += 1
     current_time = datetime.datetime.now()
-    key = f'{current_time.hour:02d}:{current_time.minute:02d}'
-    if current_time.minute in [0,15,30,45]:
+    if current_time.minute in min_conversions:
+        minute = min_conversions[current_time.minute]
+        key = f'{current_time.hour:02d}:{minute:02d}'
         if sch.get(key,None) is not None:
             if old_key != key:
                 old_key = key
-                print(key,sch.get(key,'off'))
-    if count > 47:
+                print('minute ',minute, current_time.minute,old_key,key,sch.get(key,'off'))
+    if count > 9:
         count = 0
-        with open('gti-1/gti-1.json', 'r') as f:
-            print('re-reading the file')
+        with open('gti-1/control.json', 'r') as f:
             sch = json.loads(f.read())
 
 
