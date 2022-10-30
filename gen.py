3934c852fbcb5176991ba07a33b69eb019abf771
kglavin <kglavin@gmail.com>
2022-10-29 17:20:40 -0700

    add gen.py

---
+++ gen.py
@@ -0,0 +1,30 @@
+import json
+
+sch = {}
+
+sch['00:00'] = 'off'
+sch['01:00'] = 'off'
+sch['02:00'] = 'off'
+sch['03:00'] = 'off'
+sch['04:00'] = 'off'
+sch['05:00'] = 'off'
+sch['06:00'] = 'off'
+sch['07:00'] = 'off'
+sch['08:00'] = 'off'
+sch['09:00'] = 'off'
+sch['l0:00'] = 'off'
+sch['l1:00'] = 'off'
+sch['l2:00'] = 'off'
+sch['13:00'] = 'off'
+sch['14:00'] = 'off'
+sch['16:00'] = 'off'
+sch['17:00'] = 'off'
+sch['18:00'] = 'off'
+sch['19:00'] = 'off'
+sch['20:00'] = 'off'
+sch['21:00'] = 'off'
+sch['22:00'] = 'off'
+sch['23:00'] = 'off'
+
+
+print(json.dumps(sch))
