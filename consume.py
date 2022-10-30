aba6f6d0ff8665d54291bd5a4b3ab017b8a2f89b
kglavin <kglavin@gmail.com>
2022-10-29 19:39:34 -0700

    modulo 15

--- consume.py
+++ consume.py
@@ -17,10 +17,11 @@ while True:
     count += 1
     current_time = datetime.datetime.now()
     key = f'{current_time.hour:02d}:{current_time.minute:02d}'
-
-    if old_key != key:
-        old_key = key
-        print(key,sch.get(key,'off'))
+    if current_time.minute in [0,15,30,45]:
+        if sch.get(key,None) is not None:
+            if old_key != key:
+                old_key = key
+                print(key,sch.get(key,'off'))
     if count > 47:
         count = 0
         with open('gti-1/gti-1.json', 'r') as f:
