69697392e07351b9743b195bd890c0460c3b17ff
kglavin <kglavin@gmail.com>
2022-10-29 18:10:00 -0700

    changed to hour

--- consume.py
+++ consume.py
@@ -14,7 +14,7 @@ old_key = ""
 while True:
     time.sleep(1)
     current_time = datetime.datetime.now()
-    key = f'{current_time.minute:02d}:00'
+    key = f'{current_time.hour:02d}:00'
 
     if old_key != key:
         old_key = key
