67b07848e331f35ad6beac08757fe285aab2a410
kglavin <kglavin@gmail.com>
2022-10-29 18:39:21 -0700

    read file periodically

--- consume.py
+++ consume.py
@@ -11,11 +11,19 @@ with open('gti-1/gti-1.json', 'r') as f:
 print(sch)
 
 old_key = ""
+count = 0
 while True:
     time.sleep(1)
+    count += 1
     current_time = datetime.datetime.now()
     key = f'{current_time.hour:02d}:00'
 
     if old_key != key:
         old_key = key
         print(key,sch.get(key,'off'))
+    if count > 47:
+        count = 0
+        with open('gti-1/gti-1.json', 'r') as f:
+            sch = json.loads(f.read())
+
+
