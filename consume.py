02960c172dc8041ecbe8e9bd51fc1cd84b20e5cc
kglavin <kglavin@gmail.com>
2022-10-29 17:51:48 -0700

    new client

---
+++ consume.py
@@ -0,0 +1,20 @@
+import datetime
+import json
+import time
+
+
+
+sch = {}
+with open('gti-1/gti-1.json', 'r') as f:
+  sch = json.loads(f.read())
+
+print(sch)
+
+old_key = ""
+while True:
+    time.sleep(1)
+    current_time = datetime.datetime.now()
+    key = f'{current_time.minute}:00'
+    if old_key != key:
+        old_key = key
+        print(key,sch.get(key,'off'))
